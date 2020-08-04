b1bac9f72108f03ea07937a03d557a7c54afe48f
AlshimaaRifaat <36330776+AlshimaaRifaat@users.noreply.github.com>
2020-08-04 06:50:19 +0200

    all done

--- app/src/main/java/com/example/databindinglogin/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/example/databindinglogin/ui/auth/AuthViewModel.kt
@@ -9,40 +9,5 @@ class AuthViewModel(private val userRespository: UserRepository) : ViewModel() {
         email:String,
         password:String
     ) = userRespository.userLogin(email,password)
-   /*  var email:String?=null
-    var password:String?=null
-  var authListener:AuthListener?=null
 
-    fun onLoginButtonClick(view:View)
-    {
-        authListener?.onStarted()
-        if(email.isNullOrEmpty()||password.isNullOrEmpty())
-        {
-          authListener?.onFailure("invalid email or password!")
-            return
-        }
-
-        Coroutines.main{
-
-           try {
-               val authResponse=userRespository.userLogin(email!!,password!!)
-               authResponse.user.let {
-                   authListener?.onSuccess(it)
-                   return@main
-               }
-               authListener?.onFailure(authResponse.message)
-           }catch (e:ApiException)
-           {
-               authListener?.onFailure(e.message!!)
-           }catch (e:NoInternetException)
-           {
-               authListener?.onFailure(e.message!!)
-           }
-        }
-    }
-    fun onNextTextClick(view: View){
-          Intent(view.context,HomeActivity::class.java).also {
-              view.context.startActivity(it)
-          }
-    }*/
 }
